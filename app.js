23469ab75cd67d546fbbecc39543be596681f4a7
Rohan <imgr8rohan6@gmail.com>
2021-06-06 17:37:15 +0530

    third commit

--- app.js
+++ app.js
@@ -120,11 +120,8 @@ app.get("/me/about",function(req, res){
   res.render("about")
 })
 
-let port=process.env.PORT
-if (port==null||port==""){
-  port=3000
-}
 
-app.listen(port, function() {
-  console.log("server up")
+
+app.listen(process.env.PORT||3000, function() {
+  console.log("hi")
 })
